0605e4083d4d97aed674f571db0d8638c884557b
borz <fank-1191@mail.ru>
2024-07-12 20:41:24 +0300

    update

--- src/Screens/Main/Main.tsx
+++ src/Screens/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "../../Components/Main/Header";
 import MainBanner from "../../Components/Main/MainBanner";
 import Content from "./Content";
